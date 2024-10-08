2c92edab794df8ab6499968d0fc340039f464ff7
Briar <supersecretdeer27@gmail.com>
2024-10-07 21:42:48 -0700

    create and customize admin site displays of data models

--- catalog/models.py
+++ catalog/models.py
@@ -85,6 +85,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_genre(self):
+        '''Create a string for the genre. This is required to display genre in Admin.'''
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
 class BookInstance(models.Model):
     '''Model representing a specific copy of a book (i.e. that can be checked out).'''
     id = models.UUIDField(primary_key=True, default=uuid.uuid4,
