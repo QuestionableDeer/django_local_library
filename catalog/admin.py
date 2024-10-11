046e366ef51e9fc738406a81d5cfcbefb8cb4eab
Briar <supersecretdeer27@gmail.com>
2024-10-10 22:27:23 -0700

    add support for user sessions, login/logout, and permissions

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,7 +8,7 @@ class BooksInline(admin.TabularInline):
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-    
+
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
     inlines = [BooksInline]
@@ -26,7 +26,6 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ('book', 'status', 'due_back', 'id')
-
     list_filter = ('status', 'due_back')
 
     fieldsets = (
@@ -34,7 +33,7 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
 
